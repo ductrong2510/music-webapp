9703d53d56f5483289a860a919a8257688f1cb72
Trọng Lê <ductrong03ldt@gmail.com>
2025-03-01 15:57:41 +0700

    add Suspense search page

--- src/app/(pages)/search/page.tsx
+++ src/app/(pages)/search/page.tsx
@@ -1,9 +1,12 @@
+import { Suspense } from "react"
 import Section1 from "./Section1"
 
 export default async function SearchPage() {
   return (
     <>
-      <Section1 />
+      <Suspense>
+        <Section1 />
+      </Suspense>
     </>
   )
 }
