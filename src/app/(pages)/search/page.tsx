24bdaef79c9a78d92723634af1ebb26a17ed7e16
Trọng Lê <ductrong03ldt@gmail.com>
2025-03-01 15:51:12 +0700

    fix bug deploy Search Page

--- src/app/(pages)/search/page.tsx
+++ src/app/(pages)/search/page.tsx
@@ -1,15 +1,9 @@
-"use client"
-
-import { useSearchParams } from "next/navigation"
 import Section1 from "./Section1"
 
 export default async function SearchPage() {
-  const searchParams = useSearchParams()
-  const keyword = searchParams.get("keyword") || ""
-
   return (
     <>
-      <Section1 keyword={keyword} />
+      <Section1 />
     </>
   )
 }
